7f103d0b0f81394ce4a73995ebbe732d1ce6fb50
Frank Broda <fbroda@ipb-halle.de>
2023-03-02 16:38:41 +0100

    add image version tag

--- build.sh
+++ build.sh
@@ -11,9 +11,23 @@ cleanup() {
     echo "build process has failed"
 }
 
+pushImage() {
+    IMAGE=$1
+    TAG=$2
+    DST=$IMAGE:$TAG
+
+    docker image tag $IMAGE $DST
+    docker push $DST
+}
+
 trap "cleanup; exit 1" ERR
 
+REVISION=`mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -q -DforceStdout`
+IMAGE=ipbhalle/fasta-search-service
+MAJOR=`echo $REVISION | cut -d. -f1`
+MINOR=`echo $REVISION | cut -d. -f2`
+
 cd "$DIR" 
-docker build --pull -t ipbhalle/fasta-search-service .
-docker push ipbhalle/fasta-search-service
+docker build --pull -t $IMAGE .
+pushImage $IMAGE $MAJOR
 
